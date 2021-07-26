90ea0739d122c37e49607c02f6aaffc5ac8678a6
SonicMonkey2580 <edwardmandelbaum55@gmail.com>
2021-07-26 12:30:00 -0700

    added a ball and scoring

--- pong/pong.py
+++ pong/pong.py
@@ -1,5 +1,8 @@
 import pygame
+from pygame import font
 from paddle import Paddle
+from ball import Ball
+
 pygame.init()
 
 BLACK = (0,0,0)
@@ -17,14 +20,22 @@ paddle_B = Paddle(WHITE, 10 ,100)
 paddle_B.rect.x = 670
 paddle_B.rect.y = 200
 
+ball = Ball(WHITE, 10, 10)
+ball.rect.x = 345
+ball.rect.y = 195
+
 all_sprites_list = pygame.sprite.Group()
 
 all_sprites_list.add(paddle_A)
 all_sprites_list.add(paddle_B)
+all_sprites_list.add(ball)
 
 carry_on = True 
 
 clock  = pygame.time.Clock()
+
+scoreA = 0
+scoreB = 0
 
 while carry_on:
     for event in pygame.event.get():
@@ -47,11 +58,32 @@ while carry_on:
     #Game logic should go here
     all_sprites_list.update()
 
+    
+    if ball.rect.x>=690:
+        scoreA += 1
+        ball.velocity[0] = -ball.velocity[0]
+    if ball.rect.x<=0:
+        scoreB += 1
+        ball.velocity[0] = -ball.velocity[0]
+    if ball.rect.y>490:
+        ball.velocity[1] = -ball.velocity[1]
+    if ball.rect.y<0:
+        ball.velocity[1] = -ball.velocity[1]
+
+    if pygame.sprite.collide_mask(ball, paddle_A) or pygame.sprite.collide_mask(ball, paddle_B):
+        ball.bounce()
+
     #drawing code should go here
     screen.fill(BLACK)
     pygame.draw.line(screen, WHITE, [349,0], [349, 500], 5)
 
     all_sprites_list.draw(screen)
+
+    font = pygame.font.Font(None, 74)
+    text = font.render(str(scoreA), 1, WHITE)
+    screen.blit(text, (250,10))
+    text = font.render(str(scoreB), 1, WHITE)
+    screen.blit(text,(420,10))
 
     pygame.display.flip()
 
